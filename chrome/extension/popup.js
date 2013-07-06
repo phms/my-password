607015b5f6a8bb16f8542400df935c7f640352d7
Fabio Serra <fabio.phms@gmail.com>
2013-07-06 10:37:56 -0300

    Update popup.js
    Compatibility with Chrome versions 20-25 using chrome.extension.sendMessage.

--- chrome/extension/popup.js
+++ chrome/extension/popup.js
@@ -5,7 +5,7 @@
  * Developed by @fabiophms
  * 2011-2013
  */
- 
+
 function MENU() {
 	this.host = document.getElementById("field_host");
 	this.key = document.getElementById('field_key');
@@ -13,23 +13,23 @@ function MENU() {
 	this.field2 = document.getElementById("field_2");
 	this.img = document.getElementById("img_key");
 	this.enableDoubleCheck = false;
-	
-	this.changeHostname = function(){
+
+	this.changeHostname = function () {
 		this.host.className = "";
 		this.host.readOnly = false;
 		this.host.focus();
 	};
-	
-	this.copyKeyToClipboard = function(){
+
+	this.copyKeyToClipboard = function () {
 		var type = this.key.type;
 		this.key.type = "text";
 		this.key.select();
 		document.execCommand("Copy");
 		this.key.type = type;
 	};
-	
-	this.toggleKeyFieldType = function(){
-		if (String(this.key.type).toLowerCase() === "text") {
+
+	this.toggleKeyFieldType = function () {
+		if(String(this.key.type).toLowerCase() === "text") {
 			this.key.type = "password";
 			this.img.src = "img/font.png";
 		} else {
@@ -40,11 +40,11 @@ function MENU() {
 		// Fix to IE - http://www.codingforums.com/showthread.php?t=107073
 		// function replaceT(obj){var newO=document.createElement('input');newO.setAttribute('type','password');newO.setAttribute('name',obj.getAttribute('name'));obj.parentNode.replaceChild(newO,obj);}
 	};
-	
-	this.toggleDoubleCheck = function(){
+
+	this.toggleDoubleCheck = function () {
 		var tr = document.getElementById("tr_field_2");
-		
-		if (!this.enableDoubleCheck){
+
+		if(!this.enableDoubleCheck) {
 			tr.style.display = "table-row";
 			this.key.value = "";
 			this.enableDoubleCheck = true;
@@ -55,57 +55,66 @@ function MENU() {
 			this.enableDoubleCheck = false;
 		}
 	};
-	
-	this.checkKeypass = function(){
+
+	this.checkKeypass = function () {
 		var img = document.getElementById("img_field_2");
 		img.src = (this.field1.value !== this.field2.value) ? "img/stop.png" : "img/accept.png"
 		img.style.display = "inline";
 	};
-	
-	this.generateKey = function() {
-	
+
+	this.generateKey = function () {
+
 		var f1 = this.field1.value,
 			f2 = this.field2.value,
 			msg = document.getElementById("tr_msg");
-		
+
 		msg.style.display = "none";
-	
-		if (f1 && this.enableDoubleCheck && f1 !== f2) {
+
+		if(f1 && this.enableDoubleCheck && f1 !== f2) {
 			(msg.getElementsByTagName("td")[0]).textContent = chrome.i18n.getMessage("alert_wrong_key_check");
 			msg.style.display = "table-row";
 			this.field2.value = "";
-		} else if (f1 || (this.enableDoubleCheck && f1 === f2)) {
+		} else if(f1 || (this.enableDoubleCheck && f1 === f2)) {
 			keypass4all.set(this.host.value, f1);
 			this.key.value = keypass4all.get();
-			
-			if (typeof chrome !== "undefined") {
-				chrome.tabs.executeScript(null, {code: "var keypass4all = '"+keypass4all.get()+"';", allFrames: true});
-				chrome.tabs.executeScript(null, {file: "/tabs.js", allFrames: true});
+
+			if(typeof chrome !== "undefined") {
+				chrome.tabs.executeScript(null, {
+					code: "var keypass4all = '" + keypass4all.get() + "';",
+					allFrames: true
+				});
+				chrome.tabs.executeScript(null, {
+					file: "/tabs.js",
+					allFrames: true
+				});
 			}
-		} else if (!f1) {
+		} else if(!f1) {
 			window.close();
 		}
 	};
 }
 
-if (chrome && chrome.runtime) {
-	chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+if(chrome && chrome.runtime) {
+
+	var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
+
+	chrome[runtimeOrExtension].sendMessage({ greeting: "start" }, function (response) {
 		var tab = response,
 			url = document.createElement("a");
-			
+
 		url.setAttribute("href", tab.url);
 		document.getElementById("field_host").value = String(url.host).replace(/^ww\w*\./, "");
 
-		if (tab.favIconUrl) {
+		if(tab.favIconUrl) {
 			document.getElementById("img_host").setAttribute("src", tab.favIconUrl);
 		}
-		
+
 		document.getElementById("label_host").textContent = chrome.i18n.getMessage("label_host") + ":";
 		document.getElementById("label_field_1").textContent = chrome.i18n.getMessage("label_field_1") + ":";
 		document.getElementById("label_field_2").textContent = chrome.i18n.getMessage("label_field_2") + ":";
 		document.getElementById("label_key").textContent = chrome.i18n.getMessage("label_key") + ":";
 		document.getElementById("label_button").textContent = chrome.i18n.getMessage("label_button");
-		
+
 		//window.menu = new MENU();
 		menu.field1.focus();
 
@@ -113,17 +122,19 @@ if (chrome && chrome.runtime) {
 	});
 }
 
-function addEvent(id, event, func){
-	document.getElementById(id).addEventListener(event, function(){menu[func]();});
+function addEvent(id, event, func) {
+	document.getElementById(id).addEventListener(event, function () {
+		menu[func]();
+	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 	window.menu = new MENU();
 
-	addEvent('img_host',	'click',  'changeHostname');
-	addEvent('img_field_1',	'click',  'toggleDoubleCheck');
-	addEvent('img_key',		'click',  'toggleKeyFieldType');
-	addEvent('field_2',		'change', 'checkKeypass');
-	addEvent('button',		'click',  'generateKey');
-	addEvent('img_button',	'click',  'copyKeyToClipboard');
+	addEvent('img_host', 'click', 'changeHostname');
+	addEvent('img_field_1', 'click', 'toggleDoubleCheck');
+	addEvent('img_key', 'click', 'toggleKeyFieldType');
+	addEvent('field_2', 'change', 'checkKeypass');
+	addEvent('button', 'click', 'generateKey');
+	addEvent('img_button', 'click', 'copyKeyToClipboard');
 });
